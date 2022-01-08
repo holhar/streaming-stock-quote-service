810b74d6fa84a4ab5eb33131004fd6bbb449956d
Holger Harms <holhar@posteo.de>
2022-01-08 15:08:48 +0100

    initial commit

---
+++ src/test/java/de/holhar/spring/reactive/streamingstockquoteservice/StreamingStockQuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.holhar.spring.reactive.streamingstockquoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingStockQuoteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
